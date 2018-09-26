604e680d7a468963fe87fc4ad94fac3cf5bf29f0
yungchen <yungchen@gmail.com>
2018-09-26 12:48:29 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        float v1 = scn.nextFloat();
+        v1 = (int)(v1 * 1.6f * 100) / 100f;
+        System.out.println(v1);
+    }
+}
